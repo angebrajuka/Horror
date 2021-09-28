1bb8eee0c57060fba10906263d342563cc8bdc74
angelobrajuka <brajuka.angelo@gmail.com>
2021-09-27 22:24:15 -0400

    MUSIC COOL

--- Assets/Scripts/Init.cs
+++ Assets/Scripts/Init.cs
@@ -5,10 +5,12 @@ public class Init : MonoBehaviour
     // hierarchy
     public Transform player;
     public AudioManager audioManager;
+    public MusicController musicController;
 
     void Start()
     {
         audioManager.Init();
+        musicController.Init();
         player.GetComponent<PauseHandler>().Init();
 
         player.GetComponent<PlayerInput>().Init();
---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour
+{
+    // components
+    private AudioSource audioSource;
+
+
+    private AudioClip[] clips;
+    private int clip;
+
+    public void Init()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        clips = Resources.LoadAll<AudioClip>("Audio/Ambience");
+
+        Shuffle();
+        PlayClip();
+    }
+
+    public void Shuffle()
+    {
+        AudioClip oldLast = clips[clips.Length-1];
+
+        for(int i=0; i<clips.Length-2; i++)
+        {
+            int value = Random.Range(i+1, clips.Length);
+            AudioClip temp = clips[i];
+            clips[i] = clips[value];
+            clips[value] = temp;
+        }
+
+        if(clips[0] == oldLast)
+        {
+            clips[0] = clips[1];
+            clips[1] = oldLast;
+        }
+
+        foreach(var clip in clips)
+        {
+            Debug.Log(clip);
+        }
+
+        clip = 0;
+    }
+
+    void PlayClip()
+    {
+        audioSource.clip = clips[clip];
+        audioSource.Play();
+        clip ++;
+
+        if(clip == clips.Length)
+        {
+            Shuffle();
+        }
+    }
+
+    void Update()
+    {
+        if(!audioSource.isPlaying)
+        {
+            PlayClip();
+        }
+    }
+}
