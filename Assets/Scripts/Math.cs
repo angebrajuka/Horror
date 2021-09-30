e647aac3fc2d351129b488336af3ee602b030a5f
angelobrajuka <brajuka.angelo@gmail.com>
2021-09-30 14:56:15 -0400

    added directions to Math.cs for future convenience

--- Assets/Scripts/Math.cs
+++ Assets/Scripts/Math.cs
@@ -1,5 +1,14 @@
 public static class Math
 {
+    public static readonly Vector3Int N     = new Vector3Int( 0,  0,  1);
+    public static readonly Vector3Int E     = new Vector3Int( 1,  0,  0);
+    public static readonly Vector3Int S     = new Vector3Int( 0,  0, -1);
+    public static readonly Vector3Int W     = new Vector3Int(-1,  0,  0);
+    public static readonly Vector3Int NE    = new Vector3Int( 1,  0,  1);
+    public static readonly Vector3Int SE    = new Vector3Int(-1,  0,  1);
+    public static readonly Vector3Int NW    = new Vector3Int( 1,  0, -1);
+    public static readonly Vector3Int SW    = new Vector3Int(-1,  0, -1);
+
     public static float Remap(float oldValue, float oldMin, float oldMax, float newMin, float newMax)
     {
         return newMin + (oldValue-oldMin)*(newMax-newMin)/(oldMax-oldMin);
