f0ba87c4e867429a818258742436729862ed3c6b
angelobrajuka <brajuka.angelo@gmail.com>
2021-10-01 18:27:46 -0400

    started adding enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public static class Enemy
+{
+
+    public static bool LineOfSight(Transform enemy)
+    {
+
+
+        return false;
+    }
+
+    public static void _Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemy_WeepingAngel.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using static Enemy;
+
+public class Enemy_WeepingAngel : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        _Update();
+    }
+}
