62bbf4e1b876ab249177f882d342235bfa9b2ce6
angelobrajuka <brajuka.angelo@gmail.com>
2021-10-03 16:32:33 -0400

    badass weeping angel eye glow

--- Assets/Scripts/Enemy_WeepingAngel.cs
+++ Assets/Scripts/Enemy_WeepingAngel.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Enemy_WeepingAngel : MonoBehaviour
 {
     // hierarchy
+    public MeshRenderer[] eyes;
     public OnScreen onScreen;
     public EnemyLookedAt enemyLookedAt;
     public float despawnDistance;
@@ -23,6 +24,11 @@ public class Enemy_WeepingAngel : MonoBehaviour
         {
             transform.position = Vector3.MoveTowards(transform.position, PlayerMovement.m_rigidbody.position, Time.deltaTime);
             transform.LookAt(PlayerMovement.m_rigidbody.position);
+        }
+
+        foreach(var eye in eyes)
+        {
+            eye.material.SetFloat("e", enemyLookedAt.LookedAtTimer / breakTimerMax);
         }
 
         if(enemyLookedAt.LookedAtTimer > breakTimerMax)
